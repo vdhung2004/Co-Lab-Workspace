e16d32051ab7b83fd6822e058710f27d9a3bbb4a
Vu Duy Hung <vdhung2004@gmail.com>
2025-12-04 10:42:35 +0700

    setting(fe)

--- frontend/app/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center">
-        <h1 className="text-2xl font-bold">E-commerce Store</h1>
-      </div>
-    </header>
-  );
-}
